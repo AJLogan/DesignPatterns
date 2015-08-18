14fae97fc58bd85ce91704104e1f6da42d52ac9b
Andrew Logan <andrew@ajlogan.net>
2015-08-18 10:12:11 +0100

    modified class loader to use the video example

--- CourseProjects/CourseProjects/JavaEclipseProjects/JavaLanguageExamples/src/demos/classloaders/custom/Demo.java
+++ CourseProjects/CourseProjects/JavaEclipseProjects/JavaLanguageExamples/src/demos/classloaders/custom/Demo.java
@@ -5,10 +5,10 @@ import java.lang.reflect.Method;
 
 public class Demo {
     public static void main(String[] args) throws Exception {
-        File[] paths = new File[]{new File(".." + File.separator + "GameOfLifeApplication" + File.separator + "bin")};
+        File[] paths = new File[]{new File(".." + File.separator + "UnitTestingExercises" + File.separator + "bin")};
         CustomClassLoader loader = new CustomClassLoader(paths);
         
-        Class<?> klass = Class.forName("gameoflife.Launcher",true,loader);
+        Class<?> klass = Class.forName("exercise.tdd.videostore.finish.CommandLine",true,loader);
         Method m = klass.getMethod("main", new Class[]{new String[]{}.getClass()});
         String[] data = {"abc"};
         m.invoke(null,new Object[]{data});
