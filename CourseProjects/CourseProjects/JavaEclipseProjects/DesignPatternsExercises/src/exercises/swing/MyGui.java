8ad191ac6eef21402e3879ec1b4e78849fbee112
Andrew Logan <andrew@ajlogan.net>
2015-08-18 11:43:11 +0100

    Added ActionListener to watch for button push

--- CourseProjects/CourseProjects/JavaEclipseProjects/DesignPatternsExercises/src/exercises/swing/MyGui.java
+++ CourseProjects/CourseProjects/JavaEclipseProjects/DesignPatternsExercises/src/exercises/swing/MyGui.java
@@ -1,10 +1,19 @@
 package exercises.swing;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
+
+class MyListner implements ActionListener {
+	public void actionPerformed(ActionEvent arg0){
+		System.out.println("Button Pushed");
+	}
+}
+
 
 public class MyGui extends JFrame {
 	public MyGui() {
@@ -12,6 +21,7 @@ public class MyGui extends JFrame {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
 		button = new JButton("Push Me");
+		button.addActionListener(new MyListner());
 		textField = new JTextField(10);
 		
 		setLayout(new FlowLayout());
