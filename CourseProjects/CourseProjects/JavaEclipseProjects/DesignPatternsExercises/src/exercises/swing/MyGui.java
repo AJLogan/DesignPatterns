850bff03ddb4ab54eae794b733715fd7d2c50538
Andrew Logan <andrew@ajlogan.net>
2015-08-18 11:58:46 +0100

    method references and lamdas

--- CourseProjects/CourseProjects/JavaEclipseProjects/DesignPatternsExercises/src/exercises/swing/MyGui.java
+++ CourseProjects/CourseProjects/JavaEclipseProjects/DesignPatternsExercises/src/exercises/swing/MyGui.java
@@ -8,30 +8,32 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 
-
-
-
 public class MyGui extends JFrame {
 	private class MyListner implements ActionListener {
-		public void actionPerformed(ActionEvent arg0){
+		public void actionPerformed(ActionEvent arg0) {
 			System.out.println("Button Pushed");
 			textField.setText("ButtonPushed");
 		}
 	}
-	
+
 	public MyGui() {
 		super("My Swing GUI");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
+
 		button = new JButton("Push Me");
 		button.addActionListener(new MyListner());
+		button.addActionListener(event -> System.out.println("Lamda Event Handler"));
+		button.addActionListener(this::foo);
 		textField = new JTextField(10);
-		
+
 		setLayout(new FlowLayout());
 		add(button);
 		add(textField);
 	}
-	
+
+	private void foo(ActionEvent arg0){
+		System.out.println("Handler via methdo ref");
+	}
 	private JButton button;
 	private JTextField textField;
 }
