c4b8d0bf4acf44b07cec346c54c3fe67df893663
Andrew Logan <andrew@ajlogan.net>
2015-08-19 12:03:49 +0100

    formatting

--- CourseProjects/CourseProjects/JavaEclipseProjects/JavaEightMaterials/src/demos/fp/lambdas/Converter.java
+++ CourseProjects/CourseProjects/JavaEclipseProjects/JavaEightMaterials/src/demos/fp/lambdas/Converter.java
@@ -1,6 +1,6 @@
 package demos.fp.lambdas;
 
 @FunctionalInterface
-public interface Converter<T,U> {
-    public U convert(T input);
+public interface Converter<T, U> {
+	public U convert(T input);
 }
